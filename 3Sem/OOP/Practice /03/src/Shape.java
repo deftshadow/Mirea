4cc2ef341aa9333b69f4e927669972c6518ee553
Arina Dragon <dragonarina@gmail.com>
2019-09-18 18:33:44 +0300

    completed

---
+++ 3Sem/OOP/Practice /03/src/Shape.java
@@ -0,0 +1,44 @@
+public abstract class Shape {
+    protected String color;
+    protected Boolean filled;
+
+    public Shape() {
+        color = "yellow";
+        filled = false;
+    }
+
+    public Shape(String color, Boolean filled) {
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isFilled(){
+        return filled;
+    }
+
+    public void setFilled(Boolean filled) {
+        this.filled = filled;
+    }
+
+    public abstract double  getArea();
+    public abstract double getPerrimeter();
+
+
+
+    @Override
+    public String toString() {
+        return "Shape " + color;
+    }
+
+}
+
+
+
