1bc6e54e6a94e38b085342e6f8752f745984138a
Arina Dragon <dragonarina@gmail.com>
2019-09-25 16:17:10 +0300

    completed

--- 3Sem/Practice /06/src/Student.java
+++ 3Sem/Practice /06/src/Student.java
@@ -38,4 +38,7 @@ public class Student {
 
 
 
+
+
+
 }
--- 3Sem/Practice /06/src/test.java
+++ 3Sem/Practice /06/src/test.java
@@ -10,23 +10,28 @@ public class test {
         students.add(new Student("YoungSu", "Autist", 9));
         students.add(new Student("Kostya", "Broo", 1));
         students.add(new Student("Arishka", "Dragon", 6));
-        students.sort(new Comparator<Student>() {
-            @Override
-            public int compare(Student o1, Student o2) {
-                if (o1.getID() > o2.getID())
-                    return 1;
-                if (o2.getID() == o2.getID())
-                    return 0;
-                else
-                    return -1;
+        Sortirovka(students);
 
-            }
-        });
         for (int i = 0; i < students.size(); i++)
         {
             System.out.println(students.get(i).getID());
         }
     }
 
+    public static  void Sortirovka(ArrayList<Student> students){
+        studentComparator comparator = new studentComparator();
+        for(int i = 0; i < students.size(); i++ ){
+            for(int j = 0; j<students.size()-1; j++){
+                if(comparator.compare(students.get(j) , students.get(j+1))  >0){
+                    Student buff;
+                    buff = students.get(j);
+                    students.set(j,students.get(j+1));
+                    students.set(j+1,buff);
 
+
+                }
+            }
+        }
+
+    }
 }
