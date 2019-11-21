16dcd4e088c386a81e825ac2f73786eec6429134
Arina Dragon <dragonarina@gmail.com>
2019-11-21 11:45:19 +0300

    completed

--- 3Sem/SAOD/coursework /dialog.py
+++ 3Sem/SAOD/coursework /dialog.py
@@ -9,16 +9,20 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QTableWidgetItem
-from PyQt5.QtGui import QColor
+from PyQt5.QtGui import QColor, QFont
 
 
 class DialogUI(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
-        Dialog.setFixedSize(298, 351)
+        Dialog.setFixedSize(328, 351)
         self.text = QtWidgets.QTextEdit(self)
-        self.text.setMaximumSize(80, 30)
+        self.text.setMaximumSize(100, 30)
         self.tableWidget = QtWidgets.QTableWidget(Dialog)
+        self.count_label = QtWidgets.QLabel(self)
+        self.count_label.move(216, 80)
+        self.count_label.setFixedSize(100, 30)
+        self.count_label.setFont(QFont('Times New Roman', 11))
         self.tableWidget.setGeometry(QtCore.QRect(0, 0, 211, 351))
         self.tableWidget.setObjectName("tableWidget")
         self.tableWidget.setColumnCount(0)
@@ -33,6 +37,7 @@ class DialogUI(object):
         QtCore.QMetaObject.connectSlotsByName(Dialog)
 
     def search_clicked(self):
+        count = 0
         for i in range(0, self.tableWidget.rowCount()):
             self.tableWidget.item(i, 0).setBackground(QColor(255, 255, 255))
             self.tableWidget.item(i, 1).setBackground(QColor(255, 255, 255))
@@ -40,7 +45,14 @@ class DialogUI(object):
             if self.text.toPlainText() == self.tableWidget.item(i, 0).text():
                 self.tableWidget.item(i, 0).setBackground(QColor(255, 17, 0))
                 self.tableWidget.item(i, 1).setBackground(QColor(255, 17, 0))
+                count+=1
         self.tableWidget.repaint()
+        if count == 0:
+            self.count_label.setText("Значений не найдено")
+            self.count_label.repaint()
+        else:
+            self.count_label.setText('Найдено значений: ' + str(count))
+            self.count_label.repaint()
 
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
