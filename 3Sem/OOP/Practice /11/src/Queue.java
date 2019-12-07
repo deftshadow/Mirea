c58e4f79d75b06505355d2bc7e7b4d2e7eb5a1af
Arina Dragon <dragonarina@gmail.com>
2019-12-07 23:45:23 +0300

    completed

---
+++ 3Sem/OOP/Practice /11/src/Queue.java
@@ -0,0 +1,13 @@
+public interface Queue {
+
+    void enqueue(Object element);
+    Object element();
+    Object dequeue();
+    Object remove();
+    Object peek();
+    void push(Object element);
+    int size();
+    boolean isEmpty();
+    void clear();
+    Object[] toArray();
+}
